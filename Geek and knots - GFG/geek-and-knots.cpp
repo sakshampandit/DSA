67ff37072364aa63ec937b3339e0640bd07ae71b
Saksham Pandit <95771174+sakshampandit@users.noreply.github.com>
2023-01-02 02:31:33 +0530

    Added solution - LeetHub

---
+++ Geek and knots - GFG/geek-and-knots.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution{
+public:
+    long long int t[1001][1001];
+
+    long long func(int n,int k){
+        if(k>n){
+            return 0;
+        }
+        else if(k==0 || k==n){
+            return 1;
+        }
+        else if(t[n][k]){
+            return t[n][k];
+        }
+        else{
+            return t[n][k]=(func(n-1,k)%1000000007+func(n-1,k-1)%1000000007)%1000000007;
+        }
+    }
+
+    int knots(int m, int n, int k){
+        memset(t,0,sizeof(t));
+        long long int a=func(m,k);
+        long long int b=func(n,k);
+        int ans=((a%1000000007)*(b%1000000007))%1000000007;
+        return ans;
+    }
+};
+
+
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int M, N, K;
+        cin>>M>>N>>K;
+        
+        Solution ob;
+        cout<<ob.knots(M, N, K)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
