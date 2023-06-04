ea356586d058dd31fb26360b67443408b2722319
Saksham Pandit <95771174+sakshampandit@users.noreply.github.com>
2023-06-05 01:54:56 +0530

    Added solution - LeetHub

--- Symmetric Tree - GFG/symmetric-tree.cpp
+++ Symmetric Tree - GFG/symmetric-tree.cpp
@@ -103,10 +103,10 @@ class Solution{
     public:
     // return true/false denoting whether the tree is Symmetric or not
     bool check(Node* left,Node* right)
-    { if(left==NULL||right==NULL)
-    return left==right;
+    {   if(left==NULL||right==NULL)
+        return left==right;
         if(left->data!=right->data)return false;
-        return check(left->left,right->right)&&check(right->left,left->right);
+        return check(left->left,right->right)&&check(left->right,right->left);
     }
     bool isSymmetric(struct Node* root)
     {
