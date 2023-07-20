a4c7339f985962c0d56b11b9c3a2aa67aa9574aa
Saksham Pandit <95771174+sakshampandit@users.noreply.github.com>
2023-07-20 20:14:35 +0530

    Time: 16 ms (62.11%), Space: 11.3 MB (13.16%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,11 +4,6 @@ public:
         int i=0,j=0;
         map<int,int>m;
         vector<int>v;
-       for(int i=0;i<nums.size();i++)
-       {
-           m[nums[i]]=j++;
-           
-       }
        for(int i=0;i<nums.size();i++){
         if(m.find(target-nums[i])!=m.end() && (i!=m[target-nums[i]]))
         {
@@ -16,8 +11,29 @@ public:
            v.push_back(m[target-nums[i]]);
             break;
         }
+            m[nums[i]]=j++;
            
         }
         return v;
     }
 };
+// class Solution{   
+// public:
+//     int getPairsCount(int arr[], int n, int sum) {
+//         // code here
+//         unordered_map<int,int> m;
+//         int ans = 0;
+//         int diff;
+        
+//         for(int i =0; i<n; i++)
+//         {
+//             diff = sum - arr[i];
+//             if( m[diff] )
+//             {
+//                 ans = ans + m[diff];
+//             }
+//             m[ arr[i] ] ++;
+//         }
+//         return ans;
+//     }
+// };
