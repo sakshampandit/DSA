73b39d0d714d1ec0d6b8389fe40a0753196f8337
Saksham Pandit <95771174+sakshampandit@users.noreply.github.com>
2023-07-20 20:15:41 +0530

    Time: 19 ms (54.19%), Space: 11 MB (23.84%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -5,7 +5,7 @@ public:
         map<int,int>m;
         vector<int>v;
        for(int i=0;i<nums.size();i++){
-        if(m.find(target-nums[i])!=m.end() && (i!=m[target-nums[i]]))
+        if(m.find(target-nums[i])!=m.end())
         {
            v.push_back(i);
            v.push_back(m[target-nums[i]]);
