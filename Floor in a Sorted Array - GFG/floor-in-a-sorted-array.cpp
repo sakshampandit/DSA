66de47a2084f36fc93cb291e47fed68208ea6d03
Saksham Pandit <95771174+sakshampandit@users.noreply.github.com>
2023-07-26 07:03:41 +0530

    Added solution - LeetHub

---
+++ Floor in a Sorted Array - GFG/floor-in-a-sorted-array.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+  public:
+    // Function to find floor of x
+    // n: size of vector
+    // x: element whose floor is to find
+    int findFloor(vector<long long> v, long long n, long long x){
+        
+        // Your code here
+       
+        long long  l=0,r=n-1,mid,ans=-1;
+        while(l<=r)
+        {
+            mid=l+(r-l)/2;
+            if(v[mid]< x)
+            {
+                ans=mid;
+                l=mid+1;
+            }
+            else if(v[mid]>x)
+               r=mid-1;
+            else
+            return mid; 
+        }
+        return ans;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	long long t;
+	cin >> t;
+	
+	while(t--){
+	    long long n;
+	    cin >> n;
+	    long long x;
+	    cin >> x;
+	    
+	    vector<long long> v;
+	    
+	    for(long long i = 0;i<n;i++){
+	        long long temp;
+	        cin >> temp;
+	        v.push_back(temp);
+	    }
+	    Solution obj;
+	    cout << obj.findFloor(v, n, x) << endl;
+	   
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
