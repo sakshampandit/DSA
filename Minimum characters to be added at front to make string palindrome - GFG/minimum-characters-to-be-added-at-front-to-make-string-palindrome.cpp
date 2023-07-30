484dc790a07c0b81381974937e3a93bb13125467
Saksham Pandit <95771174+sakshampandit@users.noreply.github.com>
2023-07-30 20:21:18 +0530

    Added solution - LeetHub

---
+++ Minimum characters to be added at front to make string palindrome - GFG/minimum-characters-to-be-added-at-front-to-make-string-palindrome.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+/* Driver program to test above function */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+    int minChar(string s){
+        //Write your code here
+          string rev_s = s;
+        reverse(rev_s.begin(), rev_s.end());
+        string l = s + "#" + rev_s;
+        
+        vector<int> p(l.size(), 0);
+        for (int i = 1; i < l.size(); i++) {
+            int j = p[i - 1];
+            while (j > 0 && l[i] != l[j])
+                j = p[j - 1];
+            p[i] = (j += l[i] == l[j]);
+        }
+        
+        return s.size() - p[l.size() - 1];
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    string str;
+	    cin >> str;
+	    Solution ob;
+	    int ans = ob.minChar(str);
+	    cout << ans<<endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
