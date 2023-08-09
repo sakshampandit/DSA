6608e7bcc286c4d9177ee5242c42c41285a72ccd
Saksham Pandit <95771174+sakshampandit@users.noreply.github.com>
2023-08-10 05:17:27 +0530

    Added solution - LeetHub

--- Stock span problem - GFG/stock-span-problem.cpp
+++ Stock span problem - GFG/stock-span-problem.cpp
@@ -10,28 +10,22 @@ class Solution
 {
     public:
     //Function to calculate the span of stockâ€™s price for all n days.
-    vector <int> calculateSpan(int prices[], int n)
+    vector <int> calculateSpan(int price[], int n)
     {
-       // Your code here]
+       // Your code here
        stack<pair<int,int>>s;
        vector<int>ans;
-    //   if(s.empty())
-    //       {
-    //         s.push({prices[0],1});
-    //         ans.push_back(1);
-    //       }
-            
        for(int i=0;i<n;i++)
-       {
-           int day=1;
-            while(!s.empty() && s.top().first<=prices[i])
-           {day+=s.top().second;
-           s.pop();
+       {   int day=1;
+           while(!s.empty() && s.top().first<=price[i])
+           {
+               day+=s.top().second;
+               s.pop();
            }
-           s.push({prices[i],day});
+           s.push({price[i],day});
            ans.push_back(day);
-        }
-        return ans;
+       }
+       return ans;
     }
 };
 
