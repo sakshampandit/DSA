d0fa663105261563041eb8935361a3b34ff0767e
Saksham Pandit <95771174+sakshampandit@users.noreply.github.com>
2023-08-09 09:01:53 +0530

    Added solution - LeetHub

--- Asteroid Collision - GFG/asteroid-collision.cpp
+++ Asteroid Collision - GFG/asteroid-collision.cpp
@@ -11,39 +11,48 @@ class Solution {
         stack<int>s;
         int i=0;
         while(i<n)
-        {
+        {   //if the stack is empty then push asteroid
             if(s.empty())
             {
                 s.push(asteroids[i]);
                 ++i;
             }
             else
-            {
+            {   
+                // conditions
+                // 1) if both are  + +,-- then they are in same direction
+                // 2) if stack element is negative and asteroid is positive because negative in stack means 
+                //    and asteroid in +ve they are going in opp direction
                 if((s.top()>0 && asteroids[i]>0)||(s.top()<0 && asteroids[i]<0)||(s.top()<0 && asteroids[i]>0))
                 {
                     s.push(asteroids[i]);
                     i++;
                 }
+                 // the only condition where they collide is stack is +ve and asteroid is -ve
                 else if(s.top()>0 && asteroids[i]<0)
                 {
-                    int a=s.top();
-                    int b=(asteroids[i]);
+                    int a=s.top(); // asteroid in stack
+                    int b=(asteroids[i]);// asteroids outside stack
                     s.pop();
+                    // both are equal
                     if(a==abs(b))
                     {
                         i++;
                     }
+                    // outside asteroid is bigger then we require more asteoid from stack to destroy it
                     else if(a<abs(b))
                     {
                         
                         continue;
                     }
                     else{
+                   // if stack in asteroid has destroyed outside stack then push that stack again 
                     s.push(a);    
                     i++;}
                 }
             }
         }
+        // push all the remaining asteroids in vector
         vector<int>ans;
         while(!s.empty())
         {
@@ -56,6 +65,7 @@ class Solution {
             return ans;
     }
 };
+
 
 //{ Driver Code Starts.
 
