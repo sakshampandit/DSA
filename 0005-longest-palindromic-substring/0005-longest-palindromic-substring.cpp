912d0f2adf7bb71fb63d18cc1c24476cdc1fc14b
Saksham Pandit <95771174+sakshampandit@users.noreply.github.com>
2023-08-18 03:11:42 +0530

    Time: 10 ms (92.12%), Space: 7 MB (75.34%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,53 +1,36 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n=s.size();
-        int ml=1,start=0,end=0;
-        for(int i=0;i<n-1;i++)
+        int start=0,end=1,l,r;
+        for(int i=1;i<s.size();i++)
         {
-          int l=i,r=i;
-            while(l>=0 && r<n)
-            {
-                if(s[l]==s[r])
-                {
-                    l--;
-                    r++;
-                }else
-                    break;
-            }
-            int len=r-l-1;
-            if(len>ml)
-            {
-                ml=len;
-                start=l+1;
-                end=r-1;
-                
-            }
-            
-        }
-        for(int i=0;i<n-1;i++)
-        {
-          int l=i,r=i+1;
-            while(l>=0 && r<n)
-            {
-                if(s[l]==s[r])
-                {
-                    l--;
-                    r++;
-                }else
-                    break;
-            }
-            int len=r-l-1;
-            if(len>ml)
-            {
-                ml=len;
-                start=l+1;
-                end=r-1;
-                
-            }
-            
-        }
-        return s.substr(start,ml);
         
+            l=i-1;
+            r=i;
+        while(l>=0 && r<s.size() && s[l]==s[r])
+        {
+            if(r-l+1>end)
+            {
+                start=l;
+                end=r-l+1;
+            }
+            l--;
+            r++;
+        }
+            l=i-1;
+            r=i+1;
+            while(l>=0 && r<s.size() && s[l]==s[r])
+        {   if(r-l+1>end)
+            {
+                start=l;
+                end=r-l+1;
+            }
+            l--;
+            r++;
+        }
+        }
+        
+            return s.substr(start,end);
     }
+    
 };
