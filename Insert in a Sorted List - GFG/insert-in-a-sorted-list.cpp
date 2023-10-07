51cca1f6e3a3f1c0faafa4205f9604f5fbf25a12
Saksham Pandit <95771174+sakshampandit@users.noreply.github.com>
2023-10-08 00:33:51 +0530

    Added solution - LeetHub

---
+++ Insert in a Sorted List - GFG/insert-in-a-sorted-list.cpp
@@ -0,0 +1,103 @@
+//{ Driver Code Starts
+//
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+void printList(Node* node) 
+{ 
+	while (node != NULL) { 
+		cout << node->data <<" "; 
+		node = node->next; 
+	}  
+	cout<<"\n";
+}
+
+// } Driver Code Ends
+/*
+structure of the node of the list is as
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution{
+  public:
+    // Should return head of the modified linked list
+   Node *sortedInsert(struct Node* head, int data) {
+       if(head==NULL){
+           Node* t = new Node(data);
+           return t;
+       }
+       Node* temp = new Node(data);
+       if(head->data > data){
+           temp->next = head;
+           head = temp;
+           return head;
+       }
+       
+       Node* prev = head;
+       Node* curr = head;
+        while(curr!=NULL and curr->data<=data){
+          prev=curr;
+          curr=curr->next;
+      }
+      prev->next=temp;
+      temp->next=curr;
+      return head;
+   }
+   
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+        
+		int data;
+		cin>>data;
+		
+		struct Node *head = new Node(data);
+		struct Node *tail = head;
+		for (int i = 0; i < n-1; ++i)
+		{
+			cin>>data;
+			tail->next = new Node(data);
+			tail = tail->next;
+		}
+		
+		int k;
+		cin>>k;
+		Solution obj;
+		head = obj.sortedInsert(head,k);
+		printList(head); 
+	}
+	return 0; 
+} 
+
+// } Driver Code Ends
